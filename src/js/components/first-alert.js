bcf4e528bbe437db65ac24cc500d49115b7cbef7
Dan Sazonov <p.294803@gmail.com>
2023-08-03 16:44:03 +0300

    Прописал первый алерт

--- src/js/components/first-alert.js
+++ src/js/components/first-alert.js
@@ -1,3 +1,9 @@
 document.getElementById("firstAlertBtn").onclick = function () {
   document.getElementById("firstAlertCont").classList.add("d-none");
 };
+
+document.getElementById("firstAlertBtn2").onclick = function () {
+
+  document.getElementById("noScripts").classList.add("d-none");
+  alert("Да нифига ты не разработчик!");
+};
